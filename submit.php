bd017bf87fffbc7bb5842f5db76649dc3525ac8f
sonuson01 <75201585+sonuson01@users.noreply.github.com>
2021-08-17 23:03:50 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -35,7 +35,7 @@ $client_id = "09919190"; //(your system unique id. that you can check in pay2all
                 'optional4' => ''
         );
 
-        $access_token = ""; //you have to add personal access token 
+        $access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyMDgiLCJqdGkiOiIwNDZjOGNhNTViMTJhZWNjMGQ2YWE0NzdlYmY4MmQwZmM2OWQxZTJlZjhjOTdkYWU5MTMyNDUyMzM0ZGNhMzU2MWI0NGYxMzgzNWNiZWJiZCIsImlhdCI6MTYyOTAyNTg5My43NjA3ODIwMDM0MDI3MDk5NjA5Mzc1LCJuYmYiOjE2MjkwMjU4OTMuNzYwNzg3OTYzODY3MTg3NSwiZXhwIjoxNjQ0OTIzNDkzLjcxNDA0NzkwODc4Mjk1ODk4NDM3NSwic3ViIjoiMzA1ODYiLCJzY29wZXMiOltdfQ.ijZabF62_NeZL9VEoCdZMd8EU6tQNOdPD4uojryDEDtj3GsBgn6PydBEeRhe0VGKpL_S70cWrM3AX6APo_7d1ot1ZTyL1sjvhWq7XeJ21_qRxMdFIndBwhvKR0KkFUD-ypKvdul4lCGJETbMia2CLDdS_vPLcJoIWX50U3Yr-OmfRFySk-DG27dPRIUOTIMsrxdogmBNB7YsHPl4iyEDQ_YjqfcWJDaMGxGM-AhbW4d4WIRjdMa2YFdl37sRO99tPeX3VwPKGITkAmm6nDGIvhKv_JyWjs-Bq017zAktNR82OBt5rBl0y1Y4SXNf6pYz0gzivV3ROmypJn1M1GQMBYO3SSsorF80p52ZRokcBa5SPckVrWFEr0m5ZYTMY--Iq55f9azmTwl6oPB6EAqeP65xl57aZiD2ZIOGuMQmGFUOzECv7MHPE9c1Pb1Eov2T2ukHBH_7-7sOuQKI-bK_odxtY9jtY9vZ0R7qAgq0OCNQjLk5vXbH6eP4EGx32scynBbDuR8mHi_Mh-E6ZpvDAHn7_cJ9bka0pmb8F3SwL1o8QZnp1yv_7R9B-VkoTQ-9Tail-_QX8fopj1nRbQhFDM5cGUM2hhLWxV4PkVjsAthxfOTSGFRt6PGiwYErzonQbGxVMw4p8UpBigYfPstFL5ANwR_9Vxafl5pTbLNYOKQ"; //you have to add personal access token 
 
     
 
